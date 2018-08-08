01a7af18c18f77ac838381432726e2fcd05825b5
Administrator <Administrator@WG0YX1RBIXJBB08>
2018-08-08 14:57:47 +0800

    3

--- src/main/java/com/web/dao/CrawlerBasicIndexDao.java
+++ src/main/java/com/web/dao/CrawlerBasicIndexDao.java
@@ -2,8 +2,10 @@ package com.web.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.web.pojo.CrawlerBasicIndex;
 
 public interface CrawlerBasicIndexDao {
-	List<CrawlerBasicIndex> selectBasicIndexByParam(String code, String name);
+	List<CrawlerBasicIndex> selectBasicIndexByParam(@Param("sharesCode") String code, @Param("sharesName") String name);
 }
